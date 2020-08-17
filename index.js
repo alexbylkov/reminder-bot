a0caaaf3a587cccfcb16009b62f7a510cdcd24c3
Александр Былков <a.bylkov@leko.team>
2020-08-17 11:09:46 +0700

    add first name

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-console.log(process.env.TOKEN)
-
 const { Telegraf } = require('telegraf')
 
 const bot = new Telegraf(process.env.TOKEN)
-bot.start((ctx) => ctx.reply('Здарова жопа'))
-bot.help((ctx) => ctx.reply('Отправь мне стикер'))
+bot.start((ctx) => {
+  console.log(ctx.message.chat.first_name)
+  ctx.reply(`Привет, ${ctx.message.chat.first_name}! Я бот Напоминалкин, могу ли чем-то помочь?`)
+})
+bot.use((ctx) => {
+  console.log(ctx.message)
+})
+bot.help((ctx) => ctx.reply('Я бот Напоминалкин, ты меня создал, чтобы ничего не забывать.'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.launch()
